f533ec15956c3f1c8d9e2196e83583ad9e566ecb
jmazurek <mazurek.james@live.com>
2017-06-15 15:24:55 -0400

    Added Label and Outlet to video.vc

--- partyrocking/MainVC.swift
+++ partyrocking/MainVC.swift
@@ -12,20 +12,20 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var partyRocks = [partyRock]()
+    var partyRocks = [PartyRock]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let p1 = partyRock(imageURL: "http://direct-ns.rhap.com/imageserver/v2/albums/Alb.219913217/images/600x600.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Lights Out")
+        let p1 = PartyRock(imageURL: "http://direct-ns.rhap.com/imageserver/v2/albums/Alb.219913217/images/600x600.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ev4bY1SkZLg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Lights Out")
         
-        let p2 = partyRock(imageURL: "http://www.croshalgroup.com/wp-content/uploads/2015/05/Redfoo-Website.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CdLhdrNgGu4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous")
+        let p2 = PartyRock(imageURL: "http://www.croshalgroup.com/wp-content/uploads/2015/05/Redfoo-Website.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CdLhdrNgGu4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous")
         
-        let p3 = partyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/tWyuglGCKgE\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle")
+        let p3 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/tWyuglGCKgE\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle")
         
-        let p4 = partyRock(imageURL: "https://s1-ssl.dmcdn.net/TBMPV/526x297-mk0.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/TrVLu9p65a0\" frameborder=\"0\" allowfullscreen></iframe>" , videoTitle: "Where the Sun Goes")
+        let p4 = PartyRock(imageURL: "https://s1-ssl.dmcdn.net/TBMPV/526x297-mk0.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/TrVLu9p65a0\" frameborder=\"0\" allowfullscreen></iframe>" , videoTitle: "Where the Sun Goes")
         
-        let p5 = partyRock(imageURL: "http://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/c_3xZgvy6fo\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "New Thang")
+        let p5 = PartyRock(imageURL: "http://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/c_3xZgvy6fo\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "New Thang")
         
         partyRocks.append(p1)
         partyRocks.append(p2)
--- partyrocking/PartyCell.swift
+++ partyrocking/PartyCell.swift
@@ -19,7 +19,7 @@ class PartyCell: UITableViewCell {
         
     }
 
-    func updateUI(partyRock: partyRock) {
+    func updateUI(partyRock: PartyRock) {
         videoTitle.text = partyRock.videoTitle
         
         let url = URL(string: partyRock.imageURL)
